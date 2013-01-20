cf0810b8582abcfc9d95bdf2f119f5bdc19f6c4d
bevacqua <nicolasbevacqua@gmail.com>
2013-01-20 13:59:35 -0300

    first, naive implementation

--- README.md
+++ README.md
@@ -31,3 +31,5 @@ jsn.parse(source, { how: { awesome: 'well, very much, tee-hee!' } }, function(er
 ```
 
 This is particularly useful when you have small pieces of data you want to pass to your JavaScript files. It's often most useful when paired with [**node-assetify**](https://github.com/bevacqua/node-assetify "Node.JS asset manager"), which supports **jsn** out the box.
+
+To keep things simple, JSN supports only context variable names with **[A-z], -, or _** characters.
--- src/compiler.js
+++ src/compiler.js
@@ -1,5 +1,20 @@
 function parse(source, context, cb){
-    cb(null, result);
+    var regex = /@([a-z][a-z\.-_]*)/igm;
+
+    var parsed = source.replace(regex, function(m, i){
+        var path = m.substr(1).split('.'),
+            local = context;
+
+        path.forEach(function(property){
+            if(property.length === 0){
+                throw new Error('unexpected length zero for property name');
+            }
+            local = local[property];
+        });
+        return local;
+    });
+
+    cb(null, parsed);
 }
 
 module.exports = {
